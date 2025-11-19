71f63eb3aad31a0287fc155cdfe75cae066b7c14
Nick Whitelegg <nickw4426@gmail.com>
2025-11-19 21:58:08 +0000

    Get next departures from stop by ATCO code (hardcoded Mon-Fri atm)

--- server.mjs
+++ server.mjs
@@ -66,16 +66,23 @@ app.get('/working/:analysed_block_ref', async(req, res) => {
         const dbres = await db.query("SELECT * FROM journeys WHERE analysed_block_ref=$1 ORDER BY deptime", [req.params.analysed_block_ref]);
         res.json(dbres.rows);
     } catch(e) {
-        if (e.routine == "DateTimeParseError") {
-            res.status(400).json({
-                "error":"Invalid time format for deptime"
-            });
-        } else {
-            res.status(500).json({
-                code: e.code,
-                errorType: e.routine
-            });
-        }
+        res.status(500).json({
+            code: e.code,
+            errorType: e.routine
+        });
+    }
+});
+
+// Note currently hardcoded Mon-Fri
+app.get('/stop/:atco/departures', async(req, res) => {
+    try {
+        const dbres = await db.query("SELECT * FROM (SELECT j.deptime, j.deptime + js.reltime*'1 second'::interval AS actualtime, j.route, j.journey_code, j.analysed_block_ref, j.origin, j.destination FROM journeystops js INNER JOIN stops s ON js.stopid=s.id INNER JOIN journeys j ON js.journeyid=j.id WHERE s.atco_code = $1 AND j.run_days='Mo,Tu,We,Th,Fr') AS stoptimes WHERE actualtime BETWEEN current_time AND current_time + interval '30 minutes'", [req.params.atco])
+        res.json(dbres.rows);
+    } catch(e) {
+        res.status(500).json({
+            code: e.code,
+            errorType: e.routine
+        });
     }
 });
 
